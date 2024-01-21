cd341434e9238d1d6b3183428f733510481583b5
C-Dresser <142123153+C-Dresser@users.noreply.github.com>
2024-01-21 09:55:45 -0500

    added GET route handler

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -6,6 +6,20 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
+//GET route
+app.get('/api/notes', async (req, res) => {
+    try {
+        const data = await fs.readFile('db.json', 'utf8');
+        const notes = JSON.parse(data);
+        res.json(notes);
+    }   catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+
 
 
 app.listen(PORT, () => {
