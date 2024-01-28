4578d48a6075a73b2b62a7c53b890f01bc6f6ba6
C-Dresser <142123153+C-Dresser@users.noreply.github.com>
2024-01-28 11:11:29 -0500

    fixed errors in file paths in apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,23 @@
 const router = require('express').Router();
-//const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 const fs = require ('fs');
 
 
 router.get('/api/notes', async (req, res) => {
-    const dbJson = await JSON.parse(fs.readFileSync('db/db.json'));
+    const dbJson = await JSON.parse(fs.readFileSync('db/db.json', 'utf-8'));
     res.json(dbJson);
   });
+
+router.post('/api/notes', (req, res) => {
+    const dbJson = JSON.parse(fs.readFileSync('db/db.json', 'utf-8'));
+    const userInput = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4()
+    };
+    dbJson.push(userInput);
+    fs.writeFileSync('db/db.json', JSON.stringify(dbJson));
+    res.json(dbJson);
+});
 
   module.exports = router;
